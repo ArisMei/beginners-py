45d3fa92a6789290b586b00321c49c9b8a9e6945
ArisMei <garisj98@gmail.com>
2023-03-04 13:24:03 +0200

    Second Commit

--- week_1/image_grid.py
+++ week_1/image_grid.py
@@ -1,17 +1,39 @@
+import os
+new_directory = "C:\\Users\\User\\OneDrive - Universidad Politécnica de Madrid\\Aalto\\beginners-Py\\week_1"
+os.chdir(new_directory)
 def main():
     filename = input("Enter the name of the file to be read:\n")
     try:
         # Reads content from the plaintext file
         with open(filename, "r") as file:
-        
-            # Read all the lines in the file and store them to be printed in
-            # the proper format. Remember to properly deal with incorrect lines.
-            # (hint: arrays are a good choice of data structure)
-        
-        #
-        # Print the image to the console here
-        #
-    
+            # Read the first line for image dimensions
+            try:
+                line = file.readline().strip()
+                width, height = map(int, line.split(","))
+            except (ValueError, IndexError):
+                print("Image dimensions are incorrect or the file '{:s}' is empty. Program ends.".format(filename))
+                return
+
+            # Read the remaining lines for selected positions
+            positions = set()
+            line_count = 2
+            for line in file:
+                line_count += 1
+                try:
+                    x, y = map(int, line.strip().split(","))
+                    positions.add((x, y))
+                except (ValueError, IndexError):
+                    print("Error in line: \"{}\"".format(line.strip()))
+            
+            # Print the image
+            for y in range(height):
+                for x in range(width):
+                    if (x, y) in positions:
+                        print(" ", end="")
+                    else:
+                        print("H", end="")
+                print()
+
     # Deals with file reading exceptions
     except OSError:
         print("Error in reading the file '{:s}'. Program ends.".format(filename))
